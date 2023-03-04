f75076e584c08c8448b91a62b8524ede74d9073e
denpo29 <gonza.dp29@hotmail.com>
2023-03-04 13:21:05 -0300

    Agregando el arhcivo del ejercicio

---
+++ Conjunto-Set.js
@@ -0,0 +1,13 @@
+const familia = ["María", "José", "Denis", "Yanina", "Yonatán", "Brenda"]
+
+const setFamilia = new Set(familia)
+
+console.log(setFamilia)
+
+setFamilia.add("Denis")
+
+console.log(setFamilia)
+
+setFamilia.add("JavaScript")
+
+console.log(setFamilia)
